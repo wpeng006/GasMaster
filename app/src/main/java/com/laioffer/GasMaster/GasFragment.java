e4f384b9ec166a1805982c23e95f7dc15520debd
Ziwei Jia <weigege12332@gmail.com>
2020-02-07 14:04:19 -0800

    Merge the front-end and add the fragments.

---
+++ app/src/main/java/com/laioffer/GasMaster/GasFragment.java
@@ -0,0 +1,43 @@
+package com.laioffer.GasMaster;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class GasFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+    public GasFragment() {
+
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static GasFragment newInstance(String param1, String param2) {
+        GasFragment fragment = new GasFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gas, container, false);
+    }
+}
