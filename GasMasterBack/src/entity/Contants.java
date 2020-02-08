b0ed8d81ccceac9d26b9f03a1de0063d65c4184c
wpeng006 <guixihan@gmail.com>
2020-02-08 14:09:52 -0800

    Update Contants.java
    Hide API key

--- GasMasterBack/src/entity/Contants.java
+++ GasMasterBack/src/entity/Contants.java
@@ -6,6 +6,6 @@ public class Contants
     public static final String PATH = "/maps/api/place/textsearch/json";
     public static final String DEFAULT_KEYWORD = "gas station";
     public static final int DEFAULT_RADIUS = 5000;
-    public static final String API_KEY = "AIzaSyC7MbepvrXbVQRlKsF9ECeUcTSq014v-lc";
+    public static final String API_KEY = "YOUR_API_HERE";
 
 }
