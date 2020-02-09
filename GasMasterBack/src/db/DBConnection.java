e9d8f2cbc000a158e9c02328632d2c5c4c959a6d
Weihan <guixihan@gmail.com>
2020-02-08 20:57:42 -0800

    Add database from kewei

---
+++ GasMasterBack/src/db/DBConnection.java
@@ -0,0 +1,9 @@
+package db;
+
+public interface DBConnection {
+	public void close();
+	public String getFullname(String userId);
+	public boolean verifyLogin(String userId, String password);
+	public boolean registerUser(String userId, String password, String firstname, String lastname
+			, String phone_number, String email, String car_model);
+}
