26540532f8233c52d065c7fb50da7e00d0236acc
1996XuKewei <kxu@eng.ucsd.edu>
2020-02-16 23:31:20 -0800

    add promotion and travel_complete

--- src/db/DBConnection.java
+++
@@ -1,9 +0,0 @@
-package db;
-
-public interface DBConnection {
-	public void close();
-	public String getFullname(String userId);
-	public boolean verifyLogin(String userId, String password);
-	public boolean registerUser(String userId, String password, String firstname, String lastname
-			, String phone_number, String email, String car_model);
-}
