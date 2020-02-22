0bc923fb31c42dd1b3bff4324d4b1886dba676f4
suchlowping <shao070715@gmail.com>
2020-02-22 13:59:30 -0500

    recover login functions

--- app/src/main/java/com/laioffer/GasMaster/MainActivity.java
+++ app/src/main/java/com/laioffer/GasMaster/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-//    Intent intent = new Intent(this, LoginActivity.class);
-//    startActivity(intent);
+    Intent intent = new Intent(this, LoginActivity.class);
+    startActivity(intent);
 
     bottomNavigation = findViewById(R.id.bottom_navigation);
     bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
